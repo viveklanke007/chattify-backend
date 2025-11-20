01ba6f3d90346cedf9d9b643cdcb60846ef48a9b
vivek lanke <viveklanke007@gmail.com>
2025-11-20 23:22:29 +0530

    change start script

--- db/connectDB.js
+++ db/connectDB.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDb = async () => {
-  const mongoURL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/chatify";
+  const mongoURL = process.env.MONGO_URL;
 
   try {
     await mongoose.connect(mongoURL, {
